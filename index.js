64be9c317a6349d2418a032d928c9a637b4b3637
Zach Rose <zach.rose@chromeriver.com>
2018-01-18 11:49:21 -0800

    Store last-used token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+const fs = require('fs');
 const readline = require('readline');
 const fetch = require('node-fetch');
 
@@ -8,11 +9,6 @@ const url = 'https://mydevbox.chromeriver.com/apollo/expenseReportSummaries/?lis
 
 const errorMsg = 'ooh shit';
 const successMsg = 'oooh aaah ahh ahh i m still aliiive';
-const promptMsg = [
-  'Provide a session cookie (cr_session_token_mydev)',
-  'example: 5e100b06-6cbf-467f-b71a-b70cc17a3cda'
-].join('\n').concat('\n');
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -26,20 +22,39 @@ const request = (cookie) => {
     },
     agent: false
   });
-}
+};
 
 const start = (cookie) => {
   const makeRequest = request();
   const success = () => console.log(successMsg);
   const error = () => console.error(errorMsg);
-  const loop = () => {
-    makeRequest().then(success).catch(error);
-  };
+  const loop = () => makeRequest().then(success).catch(error);
   loop();
   return setInterval(loop, interval);
-}
+};
 
-rl.question(promptMsg, (cookie) => {
+const writeToken = (cookie) => {
+  fs.writeFileSync('.token', cookie);
+};
+
+const readToken = (cookie) => {
+  try {
+    return fs.readFileSync('.token', cookie);
+  } catch(e) {
+    return undefined;
+  }
+};
+
+const promptMsg = (cookie) => {
+  return [
+    'Provide a session cookie (cr_session_token_mydev)',
+    'example: 5e100b06-6cbf-467f-b71a-b70cc17a3cda',
+    cookie ? 'or just ENTER to use your last one\n' : '\n'
+  ].join('\n');
+};
+
+rl.question(promptMsg(readToken()), (cookie) => {
+  writeToken(cookie);
   start(cookie);
   rl.close();
 });

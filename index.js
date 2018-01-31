3fce70255b455478b726965f6a861886332a4d45
bszalai <balazs.szalai@chromeriver.com>
2018-01-31 10:46:21 -0800

    one cookie for all

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
-const readline = require('readline');
 const fetch = require('node-fetch');
 
 const url = 'https://mydevbox.chromeriver.com/apollo/expenseReportSummaries/?listType=DRAFT&sortOrder=ASC&sortColumn=createDate&pagingNumItems=1&pagingStartIndex=0&pagingResetCache=true';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-rl.question('Please provide a session cookie (example: cr_session_token_mydev=5e100b06-6cbf-467f-b71a-b70cc17a3cda)', (answer) => {
-  const options = {
-    headers: {
-      cookie: answer,
-      'chain-id': 'a500-e50b-c53f-6041-112a'
-    },
-    agent: false
-  };
-  setInterval(() => {
-    fetch(url, options)
-    .catch((error) => {
-      console.log('ooh shit');
-    })
-    .then((res) => {
-      console.log('oooh aaah ahh ahh i m still aliiive');
-      console.log('status code: ', res.status);
-    })
-  }, 10000);
+const options = {
+  headers: {
+    cookie: 'cr_session_token_mydev=2d8ee8f4-55b1-4be8-b011-925d09347dc2',
+    'chain-id': 'a500-e50b-c53f-6041-112a'
+  },
+  agent: false
+};
 
 
-  rl.close();
+const makeNetworkRequest = () => {
+  console.log('Making request');
+  fetch(url, options)
+  .then((res) => {
+    console.log('oooh aaah ahh ooooahh i m still aliiive');
+    console.log('status code: ', res.status);
+  });
+};
 
-
-});
+makeNetworkRequest();
+setInterval(makeNetworkRequest, 20000);
